de38c84c8e00e9bbd6a7df94a3027b2358645a3a
Latifah President <latifahpresident@Latifahs-MacBook-Pro.local>
2019-07-22 21:52:36 -0500

    Revert "Revert "test passing for queue""
    This reverts commit f9d4fce077f8951f75a3792a0e45fd841c6dfe22.
    
    queue is working
    :wq

--- queue/queue.py
+++ queue/queue.py
@@ -1,15 +1,26 @@
+from linked_list import LinkedList
+
 class Queue:
-  def __init__(self):
-    self.size = 0
-    # what data structure should we
-    # use to store queue elements?
-    self.storage = 
+    def __init__(self):
+        self.size = 0
+        # what data structure should we
+        # use to store queue elements? * Linked List *
+        self.storage = LinkedList()
 
-  def enqueue(self, item):
-    pass
-  
-  def dequeue(self):
-    pass
+    def enqueue(self, item): # add  to tail  [item] ---> [12] ---> [23] ---> [4] head
+        self.storage.add_to_tail(item)
+        self.size += 1
 
-  def len(self):
-    pass
+    def dequeue(self): # remove and return an item from the front of the queue. removes from head
+         if self.size > 0:
+            self.size -= 1
+            return self.storage.remove_head()
+
+    def len(self):
+        return self.size
+
+linkedList = Queue()
+print(f' list: {linkedList.enqueue(15)}')
+
+
+
